e857bb8d2999dc3ceefee8f3321fa842db24d4d6
Lior Porat <lior@example.com>
2026-02-24 15:14:55 +0200

    תנו לי שקט

--- apply_cache_busting.js
+++ apply_cache_busting.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const songsDir = path.join(__dirname, 'songs');
 const files = fs.readdirSync(songsDir).filter(f => f.endsWith('.html'));
-const version = '1.0.6';
+const version = '1.0.7';
 
 files.forEach(file => {
     const filePath = path.join(songsDir, file);
